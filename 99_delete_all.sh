2b319ed53edaa6824debd1479ab726fb32dd22af
SeryioGonzalez <sergio.gonzalez.jimenez@gmail.com>
2022-09-12 15:24:17 +0200

    Update 99_delete_all.sh

--- 99_delete_all.sh
+++ 99_delete_all.sh
@@ -7,6 +7,3 @@ az account set -s $subscription_id
 
 echo "Deleting RG $rg"
 az group delete --name $rg -o none --no-wait -y 
-
-#echo "Deleting SP $az_arc_onboarding_principal_name"
-#az ad sp delete --id "$(jq -r '.appId' $az_arc_onboarding_principal_file)"
